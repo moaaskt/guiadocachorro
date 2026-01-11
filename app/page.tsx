9ed9c19e4b647a0fc8fe63dc7fea899e3942a435
moaaskt <moacirmayer@hotmail.com>
2026-01-11 15:40:11 -0300

    Add PopularBreeds component to homepage and update next.config for additional image source

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
+import { PopularBreeds } from "@/components/PopularBreeds";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <PopularBreeds />
     </>
   );
 }
